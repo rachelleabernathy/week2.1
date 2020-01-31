df9303f4f67fdb491b86db44cefc1c4c849a42b7
rachelleabernathy <57782445+rachelleabernathy@users.noreply.github.com>
2020-01-30 20:23:54 -0600

    Add files via upload

---
+++ movie.js
@@ -0,0 +1,10 @@
+const myMovies = 'Indiana Jones and the Last Crusade, Jurassic Park, Groundhog Day,';
+console.log(myMovies);
+
+userMovie = prompt('What is your favorite movie?');
+console.log(userMovie);
+
+myMovies.push(userMovie);
+console.log(userMovie);
+
+console.log(myMovies.toString());
